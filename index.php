da86399fea76f11c8d86ac89812fe6127dd7b955
robsonmonteirolacerda <robsonmonteirolacerda@gmail.com>
2022-02-13 15:50:02 -0300

    versão 1.0

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Projeto em PHP</title>
+</head>
+<body>
+
 <?php
 
-	echo "Hello word em PHP!";
+	echo "<marqueen>Hello word em PHP!</marqueen>";
 
 ?>
+
+
+</body>
+</html>
